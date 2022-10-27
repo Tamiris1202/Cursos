95b9017c82ca632e6bad799df063656b6e4e906c
Tamiris1202 <tamirisalencar50@gmail.com>
2022-10-27 18:53:43 -0300

    Exercicios de Tipos de Dados do JavaScript

---
+++ Origamid/JS-origamid/JavaScript1/TiposdeDados/script.js
@@ -0,0 +1,32 @@
+var nome = 'Tamiris';
+var nome = 21;
+console.log(typeof nome, idade);
+
+var nome = 'Tamiris';
+var sobrenome = 'Alencar';
+var nomeCompleto = nome + ' ' + sobrenome;
+
+var gols = 1000;
+var frase = 'Romário fez ' + gols + ' gols';
+
+// Declare uma variável contendo uma string
+var cidade = 'Moro na cidade de São Paulo';
+
+// Declare uma variável contendo um número dentro de uma string
+var number = '32';
+
+// Declare uma variável com a sua idade
+var idade = 21;
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var nome = 'Tamiris';
+var sobrenome = 'Alencar';
+var nomecompleto = `${nome} + ${sobrenome}`;
+
+// Coloque a seguinte frase em uma variável: It's time
+var texto = "It\'s time";
+console.log(texto);
+
+// Verifique o tipo da variável que contém o seu nome
+concole.log(typeof nome);
